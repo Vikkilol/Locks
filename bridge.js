f649888d29b73c32f95c46a192283eb4c54c8caa
Vikkilol <128256290+Vikkilol@users.noreply.github.com>
2025-02-03 23:09:50 -0500

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -27,9 +27,11 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Lock container found! Adding locks...");
 
     const locks = [
-        { id: 1, xPercent: 19, yPercent: 50, message: "Send messages to your favorite people ❤️", img: "lock1.png", size: 50 },
-        { id: 2, xPercent: 32, yPercent: 41, message: "Use a template, or use your own lock image!", img: "lock2.png", size: 65 },
-        { id: 3, xPercent: 76, yPercent: 41, message: "DM the events team when you're ready!", img: "lock3.png", size: 100 }
+        { id: 1, xPercent: 20, yPercent: 50, message: "Send messages to your favorite people ❤️", img: "lock1.png", size: 50 },
+        { id: 2, xPercent: 33, yPercent: 41, message: "Use a template, or use your own lock image!", img: "lock2.png", size: 65 },
+        { id: 3, xPercent: 73, yPercent: 41, message: "DM the events team when you're ready!", img: "lock3.png", size: 100 }
+        { id: 4, xPercent: 63, yPercent: 50, message: "DM the events team when you're ready!", img: "lock4.png", size: 50 }
+        { id: 5, xPercent: 57, yPercent: 41, message: "DM the events team when you're ready!", img: "lock5.png", size: 60 }
     ];
 
     function positionLocks() {
