a227e06db6ebd8e6fb48c3f2159e096fc5f841de
Vikkilol <128256290+Vikkilol@users.noreply.github.com>
2025-02-04 00:20:04 -0500

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const bridgeRect = bridge.getBoundingClientRect(); // Get bridge size
 
     const locks = [
-        { id: 1, xPercent: 20, yPercent: 50, message: "Send messages to your favorite people ❤️", img: "lock1.png", size: 50 },
+        { id: 1, xPercent: 0, yPercent: 0, message: "Send messages to your favorite people ❤️", img: "lock1.png", size: 50 },
         { id: 2, xPercent: 33, yPercent: 41, message: "Use a template, or use your own lock image!", img: "lock2.png", size: 65 },
         { id: 3, xPercent: 75, yPercent: 37, message: "DM the events team when you're ready!", img: "lock3.png", size: 100 },
         { id: 4, xPercent: 63, yPercent: 50, message: "DM the events team when you're ready!", img: "lock4.png", size: 100 },
