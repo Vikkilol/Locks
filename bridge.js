051f03d02e444f4b30a1abd54569c182299fa5b0
Vikkilol <128256290+Vikkilol@users.noreply.github.com>
2025-02-14 14:12:36 -0500

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -37,7 +37,7 @@ function positionLocks() {
         { id: 16, xPercent: 33, yPercent: 102, message: "❤️ badcrookshanks + Boyo ❤️", img: "lock1r.png", size: 65},
         { id: 17, xPercent: 47, yPercent: 92, message: "❤️ Harmony_Starz + crimson ❤️", img: "lock5r.png", size: 100},
         { id: 18, xPercent: 22, yPercent: 91, message: "❤️ Day + ani07 ❤️", img: "ani-day.png", size: 50},
-        { id: 19, xPercent: 15, yPercent: 91, message: "❤️Day + Muse❤️", img: "lock1r.png", size: 65}
+        { id: 19, xPercent: 13.5, yPercent: 91, message: "❤️Day + Muse❤️", img: "lock1r.png", size: 65}
         
 /*        { id: 7, xPercent: 33.75, yPercent: 95, message: "Use a template, or use your own lock image!", img: "lock1r.png", size: 65 },
         { id: 8, xPercent: 50.125, yPercent: 78, message: "You can be annonymous, or include your name!", img: "lock3r.png", size: 100},
