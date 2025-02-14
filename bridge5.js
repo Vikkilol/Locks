f1d6e30df1eeef7e6e1c8b3abe750c9f31b5100c
Vikkilol <128256290+Vikkilol@users.noreply.github.com>
2025-02-14 16:49:30 -0500

    Update bridge5.js

--- bridge5.js
+++ bridge5.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
             { id: 16, column: 7.48, yAlign: 0.8, message: "❤️ badcrookshanks + Boyo ❤️", img: "lock1r.png", size: 65 },
             { id: 17, column: 10.34, yAlign: 0.65, message: "❤️ Harmony_Starz + crimson ❤️", img: "lock5r.png", size: 100 },
             { id: 18, column: 4.84, yAlign: 0.6, message: "❤️ Day + ani07 ❤️", img: "ani-day.png", size: 50 },
-            { id: 19, column: 3.102, yAlign: 0.6, message: "❤️Day + Muse❤️", img: "lock1r.png", size: 65 },
+            { id: 19, column: 3, yAlign: 0.1, message: "❤️Day + Muse❤️", img: "lock1r.png", size: 65 },
             { id: 20, column: 14.41, yAlign: 0.85, message: "❤️To: Skyles_Daughter-----------From: Anonymous❤️", img: "lock1r.png", size: 65 },
             { id: 21, column: 18.59, yAlign: 0.85, message: "❤️Boyo + badcrookshanks❤️", img: "shanksboy.png", size: 47 },
             { id: 22, column: 18.04, yAlign: 0.6, message: "❤️zena + Ahsoka❤️", img: "vik.png", size: 85 }
