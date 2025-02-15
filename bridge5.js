f87e02e0bca8f8d25535907bebcbcc8fc8e7a285
Vikkilol <128256290+Vikkilol@users.noreply.github.com>
2025-02-14 20:33:00 -0500

    Update bridge5.js

--- bridge5.js
+++ bridge5.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
             { id: 8, column: 12.2, yAlign: 0.065, message: "❤️ pirioca + centen ❤️", img: "lock1r.png", size: 85 },
             { id: 9, column: 5.55, yAlign: 0.28, message: "❤️ kahei + Muse ❤️", img: "lock4r.png", size: 100 },
             { id: 10, column: 17.325, yAlign: 0.85, message: "❤️To: Boyo---------------From:Anonymous❤️", img: "lock5r.png", size: 130 },
-            { id: 11, column: 19.58, yAlign: 0.6, message: "❤️To: respectable082---------------From:Anonymous❤️", img: "lock1r.png", size: 85 },
+            { id: 11, column: 23, yAlign: 0.08, message: "❤️To: respectable082---------------From:Anonymous❤️", img: "lock1r.png", size: 85 },
             { id: 12, column: 13.86, yAlign: 0.85, message: "❤️ Harmony_Starz + Espoir ❤️", img: "lock5r.png", size: 130 },
             { id: 13, column: 6.6, yAlign: 0.07, message: "❤️ centen + pinkmelodies ❤️", img: "lock5r.png", size: 130 },
             { id: 14, column: 14, yAlign: 0.08, message: "❤️To: shadow---------------From:Anonymous❤️", img: "mk3.png", size: 75 },
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
             { id: 19, column: 2.8, yAlign: 0.05, message: "❤️Day + Muse❤️", img: "lock1r.png", size: 85 },
             { id: 20, column: 14.41, yAlign: 0.85, message: "❤️To: Skyles_Daughter-----------From: Anonymous❤️", img: "lock1r.png", size: 65 },
             { id: 21, column: 18.59, yAlign: 0.85, message: "❤️Boyo + badcrookshanks❤️", img: "shanksboy.png", size: 47 },
-            { id: 22, column: 18.04, yAlign: 0.6, message: "❤️zena + Ahsoka❤️", img: "vik.png", size: 85 }
+            { id: 22, column: 21, yAlign: 0.08, message: "❤️zena + Ahsoka❤️", img: "vik.png", size: 95 }
         ];
 
         locks.forEach(lock => {
