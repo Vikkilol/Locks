c40f344c8108e422ca9a52fa7b18747919701e8f
Vikkilol <128256290+Vikkilol@users.noreply.github.com>
2025-02-14 20:06:18 -0500

    Update bridge5.js

--- bridge5.js
+++ bridge5.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
             { id: 10, column: 17.325, yAlign: 0.85, message: "❤️To: Boyo---------------From:Anonymous❤️", img: "lock5r.png", size: 130 },
             { id: 11, column: 19.58, yAlign: 0.6, message: "❤️To: respectable082---------------From:Anonymous❤️", img: "lock1r.png", size: 85 },
             { id: 12, column: 13.86, yAlign: 0.85, message: "❤️ Harmony_Starz + Espoir ❤️", img: "lock5r.png", size: 130 },
-            { id: 13, column: 7.3, yAlign: 0.09, message: "❤️ centen + pinkmelodies ❤️", img: "lock5r.png", size: 130 },
+            { id: 13, column: 7.1, yAlign: 0.07, message: "❤️ centen + pinkmelodies ❤️", img: "lock5r.png", size: 130 },
             { id: 14, column: 12.54, yAlign: 0.6, message: "❤️To: shadow---------------From:Anonymous❤️", img: "mk3.png", size: 60 },
             { id: 15, column: 9.13, yAlign: 0.85, message: "❤️To: velvet---------------From:Anonymous❤️", img: "mk3.png", size: 60 },
             { id: 16, column: 7.6, yAlign: 0.3, message: "❤️ badcrookshanks + Boyo ❤️", img: "lock1r.png", size: 85 },
